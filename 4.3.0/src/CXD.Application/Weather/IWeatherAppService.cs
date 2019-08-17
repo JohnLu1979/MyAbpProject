7b6f7e5565ae5df4cc9c67b7fa777f7e69b78e2f
John.lu <John.lu.1979@outlook.com>
2019-08-17 20:47:00 +0800

    fix class name weather

--- 4.3.0/src/CXD.Application/Weather/IWeatherAppService.cs
+++ 4.3.0/src/CXD.Application/Weather/IWeatherAppService.cs
@@ -1,12 +1,13 @@
 ﻿using Abp.Application.Services;
 using CXD.Base.Dto;
 using CXD.Weather.Dto;
+using System.Threading.Tasks;
 
 namespace CXD.Weather
 {
     public interface IWeatherAppService : IApplicationService
     {
-        CDataResults<CWeatherDto> GetWeathers(CWeatherInput input);
+        Task<CDataResults<CWeatherDto>> GetWeathers(CWeatherInput input);
         CDataResult<int> AddWeather(CWeatherInput input);
         CDataResult<CWeatherDto> UpdateWeather(CWeatherInput input);
         CDataResult<int> DeleteWeather(CWeatherInput input);
