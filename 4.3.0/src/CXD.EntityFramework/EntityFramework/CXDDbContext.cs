e4b1fd872e506b8281e09a70d985cd06db064efc
Eric <yjp1314@126.com>
2019-08-18 12:25:32 +0800

    commit

--- 4.3.0/src/CXD.EntityFramework/EntityFramework/CXDDbContext.cs
+++ 4.3.0/src/CXD.EntityFramework/EntityFramework/CXDDbContext.cs
@@ -17,6 +17,8 @@ namespace CXD.EntityFramework
         public virtual IDbSet<CAccount> Accounts { get; set; }
 
         public virtual IDbSet<CWeather> CWeatherObjects { get; set; }
+
+
         /* NOTE: 
          *   Setting "Default" to base class helps us when working migration commands on Package Manager Console.
          *   But it may cause problems when working Migrate.exe of EF. If you will apply migrations on command line, do not
