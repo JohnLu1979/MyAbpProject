36a1bdcb481d0e4e40bff20dfdc0ba187fbb73a9
Eric <yjp1314@126.com>
2019-08-21 22:20:52 +0800

    commit

--- 4.3.0/src/CXD.Core/Entities/CAccount.cs
+++ 4.3.0/src/CXD.Core/Entities/CAccount.cs
@@ -10,7 +10,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CXD.Entities
 {
-    [Table("Account")]
+    [Table("zzd_Accounts")]
     public class CAccount : FullAuditedEntity<int>
     {
         [MaxLength(50)]
