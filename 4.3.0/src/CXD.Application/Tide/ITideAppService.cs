46e950368585afccb07a59a5a59843afd09001a3
John.lu <John.lu.1979@outlook.com>
2019-08-24 22:56:08 +0800

    tide service

---
+++ 4.3.0/src/CXD.Application/Tide/ITideAppService.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services;
+using CXD.Base.Dto;
+using CXD.Tide.Dto;
+using System.Threading.Tasks;
+
+namespace CXD.Tide
+{
+    public interface ITideAppService : IApplicationService
+    {
+        Task<CDataResults<CTideDto>> GetTides(CTideInput input);
+        CDataResult<int> AddTide(CTideInput input);
+        CDataResult<CTideDto> UpdateTide(CTideInput input);
+        CDataResult<int> DeleteTide(CTideInput input);
+        CDataResult<CTideDto> GetTide(CTideInput input);
+        CDataResult<string> ExportTides();
+        CDataResult<bool> ImportTides(CTideInput input);
+    }
+}
